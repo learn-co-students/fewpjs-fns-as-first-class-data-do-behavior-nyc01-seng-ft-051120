8d3b4fd6fe7d16ac2279a17e0214f98d65271031
grantkoji <grantkoji@gmail.com>
2020-06-22 14:11:31 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,28 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
+
+
+
+function greet(string) {
+  //HH:MM
+  const hours_time = string.split(":")
+  const hours = parseInt(hours_time[0], 10)   
+  if ( hours < 12 ) {
+    return 'Good Morning'
+
+    }
+  else if ( hours > 17 ) {
+    return 'Good Evening'
+  }
+  else {
+    return 'Good Afternoon'
+    }
+}
+
+function displayMessage(string) {
+  const base = document.getElementById('greeting');
+  base.innerText = string 
+}
