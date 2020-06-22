bfaef10d01d190ed352e455e2ad26f9c9000f047
cathjime <xcatherine.jimenez@gmail.com>
2020-06-22 19:38:38 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(string){
+  let time = parseInt(string);
+  if (time < 12){
+    return "Good Morning";
+  } else if (time >= 12 && time <= 17){
+    return "Good Afternoon";
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(string){
+ document.getElementById("greeting").innerText = string; 
+}
