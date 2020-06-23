eb94fe0ae4cdf2ab762cf944bfb37a41bec8b1a3
Gary Cordero <GaryCordero1690@gmail.com>
2020-06-23 12:09:08 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
+function greet(time){
+
+  time =  parseInt(time);
+  if (time < 12) {
+    return "Good Morning";
+  }
+  if (time < 18){
+    return "Good Afternoon";
+  }
+  return "Good Evening";
+}
+
+function displayMessage(msg) {
+  let element = document.querySelector("#greeting");
+  element.innerText = msg;
+}
